0a8d4a7daa0696d35a29369afa01d880f34c81b3
roman zinchuk <zigzagforever@mail.ru>
2017-02-28 12:24:12 +0200

    user registration using real database

--- cources.server/softgroup/basis/common-database/src/main/java/com/softgroup/common/database/services/UserProfileService.java
+++ cources.server/softgroup/basis/common-database/src/main/java/com/softgroup/common/database/services/UserProfileService.java
@@ -3,12 +3,14 @@ package com.softgroup.common.database.services;
 import com.softgroup.common.database.model.UserProfile;
 import com.softgroup.common.database.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by zigza on 20.02.2017.
  */
+@Service
 public class UserProfileService {
     @Autowired
     UserProfileRepository profileRepository;
--- cources.server/softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorithation/impl/handler/RegistrationRequestHandler.java
+++ cources.server/softgroup/services/authorization/authorization-impl/src/main/java/com/softgroup/authorithation/impl/handler/RegistrationRequestHandler.java
@@ -3,10 +3,12 @@ package com.softgroup.authorithation.impl.handler;
 import com.softgroup.authorization.api.message.RegistrationRequestData;
 import com.softgroup.authorization.api.message.RegistrationResponseData;
 import com.softgroup.authorization.api.router.AuthorizationRequestHandler;
+import com.softgroup.common.database.services.UserProfileService;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.protocol.ResponseStatus;
 import com.softgroup.common.router.api.AbstractRequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,6 +16,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RegistrationRequestHandler extends AbstractRequestHandler<RegistrationRequestData,RegistrationResponseData> implements AuthorizationRequestHandler {
+    @Autowired
+    UserProfileService userProfileService;
+
     @Override
     public String getName() {
         return "register";
@@ -21,14 +26,16 @@ public class RegistrationRequestHandler extends AbstractRequestHandler<Registrat
 
     public Response<RegistrationResponseData> process(Request<RegistrationRequestData> request) {
         //TODO
-        RegistrationResponseData data = new RegistrationResponseData();
-        data.setAuthCode("aabbcc");
-        data.setRegistrationRequestUuid("121343434343");
-        data.setRegistrationTimeoutSec(1000);
+        RegistrationRequestData requestData = request.getData();
+        userProfileService.register(requestData.getPhoneNumber(),requestData.getLocaleCode(),requestData.getDeviceId());
+
+        RegistrationResponseData responseData = new RegistrationResponseData();
+        responseData.setAuthCode("not set");
+        responseData.setRegistrationRequestUuid("not set");
+        responseData.setRegistrationTimeoutSec(-1);
         Response<RegistrationResponseData> response = new Response<RegistrationResponseData>();
         response.setHeader(request.getHeader());
-        response.setData(data);
-
+        response.setData(responseData);
         ResponseStatus status = new ResponseStatus();
         status.setCode(200);
         status.setMessage("OFIGENNO !!!!");
