155fdf01abe47dcfd2068c249996c9303969f37e
roman zinchuk <zigzagforever@mail.ru>
2017-02-28 13:22:32 +0200

    authorization api classes moved to com.softgroup.services.authorization, added profile api module

---
+++ cources.server/softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/services/authorization/api/message/response/RegistrationResponseData.java
@@ -0,0 +1,39 @@
+package com.softgroup.authorization.api.message.response;
+
+import com.softgroup.common.protocol.ResponseData;
+
+/**
+ * @author odin
+ * @since 20.02.17.
+ */
+public class RegistrationResponseData implements ResponseData {
+	private static final long serialVersionUID = -5146888202653750948L;
+
+	public String getRegistrationRequestUuid() {
+		return registrationRequestUuid;
+	}
+
+	public void setRegistrationRequestUuid(String registrationRequestUuid) {
+		this.registrationRequestUuid = registrationRequestUuid;
+	}
+
+	public Integer getRegistrationTimeoutSec() {
+		return registrationTimeoutSec;
+	}
+
+	public void setRegistrationTimeoutSec(Integer registrationTimeoutSec) {
+		this.registrationTimeoutSec = registrationTimeoutSec;
+	}
+
+	public String getAuthCode() {
+		return authCode;
+	}
+
+	public void setAuthCode(String authCode) {
+		this.authCode = authCode;
+	}
+
+	private String registrationRequestUuid;
+	private Integer registrationTimeoutSec;
+	private String authCode;
+}
