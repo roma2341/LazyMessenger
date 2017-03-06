808f230465df4d72711ef5614c740a3fbef5757d
roman zinchuk <zigzagforever@mail.ru>
2017-03-07 00:36:48 +0200

    get current user profile handler fixed

--- cources.server/softgroup/basis/common-database/src/main/java/com/softgroup/common/database/repository/CommonOperationsRepository.java
+++ cources.server/softgroup/basis/common-database/src/main/java/com/softgroup/common/database/repository/CommonOperationsRepository.java
@@ -2,6 +2,7 @@ package com.softgroup.common.database.repository;
 
 import com.softgroup.common.database.model.UserProfile;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Created by zigza on 06.03.2017.
  */
+@NoRepositoryBean
 public interface CommonOperationsRepository<T,ID extends Serializable> extends CrudRepository<T,ID> {
     public List<T> findAll();
     public List<T> findByIdIn(List<ID> items);
